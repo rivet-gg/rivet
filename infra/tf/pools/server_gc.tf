d1c87c72e0fb3c6631d84c67004e870c149885fe
MasterPtato <maxpopkov.contact@gmail.com>
2023-08-04 17:09:48 +0000

    Fix terraform config

--- infra/tf/pools/server_gc.tf
+++ infra/tf/pools/server_gc.tf
@@ -4,13 +4,11 @@ resource "null_resource" "server_gc" {
 	]
 
 	triggers = {
-		servers = var.servers
+		servers = join(",", keys(var.servers))
 	}
-
-	master_host = var.deploy_method_cluster ? data.terraform_remote_state.master_cluster[0].outputs.salt_master_host : null
 	
 	provisioner "local-exec" {
 		interpreter = ["/usr/bin/env", "-i", "bash", "-c"]
-		command = "salt-key -L | grep -v ':' | grep -vx \"${join(keys(var.servers), "\\|")}\" | xargs -L 1 salt-key -d"
+		command = "salt-key -L | grep -v ':' | grep -vx \"${join("\\|", keys(var.servers))}\" | xargs -L 1 salt-key -d"
 	}
 }
