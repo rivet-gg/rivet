885a589803aebcad7153881f636031e18e8f806e
Nathan Flurry <git@nathanflurry.com>
2023-08-06 08:31:45 +0000

    Add bolt headless reading secrets

---
+++ infra/tf/modules/secrets/scripts/get_secret.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -euf
+
+export BOLT_HEADLESS=1
+
+KEY="$1"
+OPTIONAL="$2"
+
+if [ "$OPTIONAL" == "true" ]; then
+	bolt secret get --format=json --optional "$KEY"
+else
+	bolt secret get --format=json "$KEY"
+fi
+
