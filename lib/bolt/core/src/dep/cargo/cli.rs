90b99d39e958caef2aa916a132ae3ca39b4d33b2
Nathan Flurry <git@nathanflurry.com>
2023-08-26 09:31:34 +0000

    Fix prod deploys

--- lib/bolt/core/src/dep/cargo/cli.rs
+++ lib/bolt/core/src/dep/cargo/cli.rs
@@ -77,7 +77,8 @@ pub async fn build<'a, T: AsRef<str>>(ctx: &ProjectContext, opts: BuildOpts<'a, 
 		r#"
 		# TODO: Not sure why the .cargo/config.toml isn't working with nested projects, have to hardcode
 		# the target dir
-		export CARGO_TARGET_DIR={target}
+		export CARGO_TARGET_DIR=$(readlink -f ./target)
+		echo "CARGO_TARGET_DIR=$CARGO_TARGET_DIR"
 		# Used for Tokio Console. See https://github.com/tokio-rs/console#using-it
 		export RUSTFLAGS="--cfg tokio_unstable"
 		# Used for debugging
@@ -87,7 +88,6 @@ pub async fn build<'a, T: AsRef<str>>(ctx: &ProjectContext, opts: BuildOpts<'a, 
 
 		EXIT_CODE=$?
 		"#,
-		target = ctx.path().join("target").display(),
 	);
 
 	// Execute build command
@@ -117,7 +117,7 @@ pub async fn build<'a, T: AsRef<str>>(ctx: &ProjectContext, opts: BuildOpts<'a, 
 			cmd.arg("--rm")
 				.arg("--interactive")
 				.arg("--tty")
-				.arg("clux/muslrust:1.71.1-stable");
+				.arg("clux/muslrust:1.72.0-stable");
 			cmd.arg("sh").arg("-c").arg(indoc::formatdoc!(
 				r#"
 				# HACK: Link musl-g++
