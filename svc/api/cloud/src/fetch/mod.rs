c304d511a13d935cc275c59c33010acd1869b923
Nathan Flurry <git@nathanflurry.com>
2023-08-29 04:39:46 +0000

    Fmt

--- svc/api/cloud/src/fetch/mod.rs
+++ svc/api/cloud/src/fetch/mod.rs
@@ -126,10 +126,8 @@ pub async fn game_summary_fetch(
 			display_name: game.display_name.clone(),
 			developer_group_id: developer_team_id.to_string(),
 			total_player_count: total_player_count.try_into()?,
-			logo_url: util::route::game_logo(
-				&game),
-			banner_url: util::route::game_banner(
-				&game),
+			logo_url: util::route::game_logo(&game),
+			banner_url: util::route::game_banner(&game),
 		});
 	}
 
