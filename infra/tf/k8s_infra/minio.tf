2abeafbcaa7a0f024864b1f5fbc135143cff1fd6
Nathan Flurry <git@nathanflurry.com>
2023-09-07 20:19:45 +0000

    Switch to bitnami minio

--- infra/tf/k8s_infra/minio.tf
+++ infra/tf/k8s_infra/minio.tf
@@ -20,37 +20,21 @@ resource "helm_release" "minio" {
 
 	name = "minio"
 	namespace = kubernetes_namespace.minio.metadata.0.name
-	repository = "https://charts.min.io/"
+	repository = "oci://registry-1.docker.io/bitnamicharts"
 	chart = "minio"
-	version = "5.0.13"
+	version = "12.8.3"
 	values = [yamlencode({
-		replicas = 1
-		mode = "standalone"
-
-		existingSecret = "minio-auth"
-		
-		minioAPIPort = 9200
-		minioConoslePort = 9201
-		service = {
-			port = 9200
-		}
-		consoleService = {
-			port = 9201
+		replicaCount = 1
+		auth = {
+			rootUser = module.minio_secrets.values["s3/minio/root/key_id"]
+			rootPassword = module.minio_secrets.values["s3/minio/root/key"]
+			service = {
+				nodePorts = {
+					api = 92000
+					console = 9201
+				}
+			}
 		}
 	})]
 }
 
-resource "kubernetes_secret" "minio_auth" {
-	depends_on = [kubernetes_namespace.minio]
-	count = local.has_minio ? 1 : 0
-
-	metadata {
-		namespace = kubernetes_namespace.minio.metadata.0.name
-		name = "minio-auth"
-	}
-
-	data = {
-		rootUser = module.minio_secrets.values["s3/minio/root/key_id"]
-		rootPassword = module.minio_secrets.values["s3/minio/root/key"]
-	}
-}
