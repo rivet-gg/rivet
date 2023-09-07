0a8b1767e17189b24c28b908dd843dc5c5cc72dc
Nathan Flurry <git@nathanflurry.com>
2023-09-07 03:46:00 +0000

    Update docker.nix

--- infra/docker/traffic_server/docker.nix
+++ infra/docker/traffic_server/docker.nix
@@ -1,12 +1,60 @@
+# nix-build docker.nix --option system-features kvm
+
+# let
+# 	pkgs = import ../../nix/common/pkgs.nix;
+# 	image = pkgs.dockerTools.buildLayeredImage {
+# 		name = "apache-traffic-server";
+# 		tag = "latest";
+
+# 		fromImage = "debian:12.1";
+
+# 		contents = [ pkgs.trafficserver ];
+
+# 		extraCommands = ''
+# 			#!${pkgs.runtimeShell}
+# 			${pkgs.dockerTools.shadowSetup}
+
+# 			groupadd -r trafficserver
+# 			useradd -r -g trafficserver trafficserver
+# 			mkdir -p /var/log/trafficserver /run/trafficserver
+# 			chown -R trafficserver:trafficserver /var/log/trafficserver /run/trafficserver
+# 		'';
+
+# 		config = {
+# 			Entrypoint = [ "${pkgs.trafficserver}/bin/traffic_server" ];
+# 			User = "1000:1000";
+# 		};
+# 	};
+# in image
+
 let
 	pkgs = import ../../nix/common/pkgs.nix;
-	image = pkgs.dockerTools.buildLayeredImage {
+	image = pkgs.dockerTools.buildImage {
 		name = "apache-traffic-server";
 		tag = "latest";
-		contents = [ pkgs.trafficserver ];
+
+		fromImageName = "debian";
+		fromImageTag = "12.1";
+
+		copyToRoot = pkgs.buildEnv {
+			name = "image-root";
+			paths = [ pkgs.trafficserver ];
+			pathsToLink = [ "/bin" ];
+		};
+
+		runAsRoot = ''
+			#!${pkgs.runtimeShell}
+			${pkgs.dockerTools.shadowSetup}
+
+			groupadd -r trafficserver
+			useradd -r -g trafficserver trafficserver
+			mkdir -p /var/log/trafficserver /run/trafficserver /etc/trafficserver
+			chown -R trafficserver:trafficserver /var/log/trafficserver /run/trafficserver /etc/trafficserver
+		'';
+
 		config = {
-			# Cmd = [ "${pkgs.trafficserver}/bin/traffic_server" ];
 			Entrypoint = [ "${pkgs.trafficserver}/bin/traffic_server" ];
+			User = "trafficserver:trafficserver";
 		};
 	};
 in image
