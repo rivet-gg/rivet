2dfa159648c37fbf890b807cc6fa039bc8821788
Nathan Flurry <git@nathanflurry.com>
2023-09-10 20:19:47 +0000

    WIP PVC example working

--- infra/tf/k8s_aws/k8s.tf
+++ infra/tf/k8s_aws/k8s.tf
@@ -1,15 +1,3 @@
-provider "kubernetes" {
-	host = module.eks.cluster_endpoint
-	cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-	# Dynamically access token
-	exec {
-		api_version = "client.authentication.k8s.io/v1beta1"
-		command = "aws"
-		args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-	}
-}
-
 resource "kubernetes_namespace" "app_test" {
 	metadata {
 		name = "app-test"
@@ -28,64 +16,184 @@ resource "kubernetes_namespace" "test_ns" {
 	}
 }
 
-# resource "kubernetes_pod" "hello_world_pod" {
+# resource "kubernetes_persistent_volume_claim" "example_pvc" {
 # 	metadata {
-# 		name = "hello-world-pod"
-# 		namespace = kubernetes_namespace.backend.metadata.0.name
-
-# 		annotations = {
-# 			# CapacityProvisioned = "0.25vCPU 0.5GB"
-# 			# "eks.amazonaws.com/compute-type" = "Fargate"
-# 		}
+# 		name = "my-efs-pvc"
+# 		namespace = kubernetes_namespace.test_ns.metadata.0.name
 # 	}
-
 # 	spec {
-# 		container {
-# 			image = "busybox"
-# 			name  = "hello-world-container"
-
-# 			command = ["sh", "-c", "while true; do echo 'Hello, World!'; sleep 1; done"]
+# 		access_modes = ["ReadWriteMany"]
+# 		# TODO: Is this needed?
+# 		# storage_class_name = "gp2"
+# 		# TODO: Is this needed?
+# 		# volume_binding_mode = "Immediate"
+# 		resources {
+# 			requests = {
+# 				storage = "5Gi"
+# 			}
 # 		}
 # 	}
 # }
 
-resource "kubernetes_deployment" "example" {
+# resource "kubernetes_deployment" "example" {
+# 	metadata {
+# 		name = "nginx-deployment"
+# 		namespace = kubernetes_namespace.test_ns.metadata.0.name
+# 		labels = {
+# 			app = "nginx"
+# 		}
+# 	}
+
+# 	spec {
+# 		replicas = 1
+
+# 		selector {
+# 			match_labels = {
+# 				app = "nginx"
+# 			}
+# 		}
+
+# 		template {
+# 			metadata {
+# 				labels = {
+# 					app = "nginx"
+# 				}
+# 			}
+
+# 			spec {
+# 				container {
+# 					name  = "nginx"
+# 					image = "nginx:1.19.3"
+
+# 					port {
+# 						container_port = 80
+# 					}
+
+# 					volume_mount {
+# 						mount_path = "/var/storage"
+# 						name       = "my-storage"
+# 					}
+# 				}
+
+# 				volume {
+# 					name = "my-storage"
+
+# 					persistent_volume_claim {
+# 						claim_name = kubernetes_persistent_volume_claim.example_pvc.metadata[0].name
+# 					}
+# 				}
+# 			}
+# 		}
+# 	}
+# }
+
+# =======================
+
+# resource "kubernetes_persistent_volume" "example_pvc" {
+# 	metadata {
+# 		name      = "my-my"
+# 		# namespace = kubernetes_namespace.test_ns.metadata[0].name
+# 	}
+# 	spec {
+# 		capacity = {
+# 			storage = "10Gi"
+# 		}
+# 		volume_mode = "Filesystem"
+# 		access_modes = ["ReadWriteMany"]
+# 		persistent_volume_reclaim_policy = "Retain"
+# 		storage_class_name = "efs-sc"
+# 		persistent_volume_source {
+# 			csi = {
+# 				driver = "efs.csi.aws.com"
+# 				volume_handle = "fs-01e83fe2a11ae8419"
+# 			}
+# 		}
+# 	}
+# }
+
+# TODO: Storage class
+
+resource "kubernetes_csi_driver_v1" "efs" {
 	metadata {
-		name = "nginx-deployment"
-		namespace = kubernetes_namespace.test_ns.metadata.0.name
-		labels = {
-			app = "nginx"
-		}
+		name = "efs.csi.aws.com"
 	}
 
 	spec {
-		replicas = 2
+		attach_required = false
+	}
+}
 
-		selector {
-			match_labels = {
-				app = "nginx"
-			}
+resource "kubernetes_storage_class" "efs_sc" {
+	metadata {
+		name = "efs-sc"
+	}
+	storage_provisioner = kubernetes_csi_driver_v1.efs.metadata.0.name
+	allow_volume_expansion = true
+	reclaim_policy = "Retain"
+}
+
+resource "kubernetes_persistent_volume" "example_pv" {
+	metadata {
+		name = "my-pv"
+	}
+
+	spec {
+		capacity = {
+			storage = "10Gi"
 		}
-
-		template {
-			metadata {
-				labels = {
-					app = "nginx"
-				}
-			}
-
-			spec {
-				container {
-					name  = "nginx"
-					image = "nginx:1.19.3"
-
-					port {
-						container_port = 80
-					}
-
-					resources {}
-				}
+		volume_mode = "Filesystem"
+		access_modes = ["ReadWriteMany"]
+		persistent_volume_reclaim_policy = "Retain"
+		storage_class_name = kubernetes_storage_class.efs_sc.metadata.0.name
+		persistent_volume_source {
+			csi {
+				driver = "efs.csi.aws.com"
+				volume_handle = "fs-01e83fe2a11ae8419"
 			}
 		}
 	}
 }
+
+resource "kubernetes_persistent_volume_claim" "example_pvc" {
+	metadata {
+		name      = "my-pvc"
+		# namespace = kubernetes_namespace.test_ns.metadata[0].name
+	}
+	spec {
+		access_modes = ["ReadWriteMany"]
+		storage_class_name = kubernetes_storage_class.efs_sc.metadata.0.name
+		volume_name = kubernetes_persistent_volume.example_pv.metadata.0.name
+		resources {
+			requests = {
+				storage = "10Gi"
+			}
+		}
+	}
+}
+
+# resource "kubernetes_pod" "example_pod" {
+# 	metadata {
+# 		name      = "hello-world-pod"
+# 		namespace = kubernetes_namespace.test_ns.metadata[0].name
+# 	}
+# 	spec {
+# 		container {
+# 			image = "busybox"
+# 			name  = "hello-container"
+# 			command = ["/bin/sh", "-c", "echo Hello, World! > /mnt/hello_world.txt; sleep 999999"]
+
+# 			volume_mount {
+# 				name       = "hello-world-storage"
+# 				mount_path = "/mnt"
+# 			}
+# 		}
+
+# 		volume {
+# 			name = "hello-world-storage"
+# 			persistent_volume_claim {
+# 				claim_name = kubernetes_persistent_volume_claim.example_pvc.metadata[0].name
+# 			}
+# 		}
+# 	}
+# }
+
