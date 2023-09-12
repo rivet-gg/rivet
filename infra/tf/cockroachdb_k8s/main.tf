4f19b9b1f9adabea69ae8a643fb41ac1b8459f45
Nathan Flurry <git@nathanflurry.com>
2023-09-12 03:01:38 +0000

    Impl cockroach k8s

--- infra/tf/cockroachdb_k8s/main.tf
+++ infra/tf/cockroachdb_k8s/main.tf
@@ -4,12 +4,18 @@ resource "kubernetes_namespace" "cockroachdb" {
 	}
 }
 
+resource "random_password" "root_password" {
+	length = 32
+	special = false
+}
+
+# NOTE: Helm chart is no longer supported by CockroachDB. However, it's intended to be used only for development and it's the easiest to set up.
 resource "helm_release" "cockroachdb" {
 	name = "cockroachdb"
 	namespace = kubernetes_namespace.cockroachdb.metadata.0.name
 	repository = "https://charts.cockroachdb.com/"
 	chart = "cockroachdb"
-	version = "11.1.5"
+	version = "11.1.5"  # v23.1.9
 	values = [yamlencode({
 		statefulset = {
 			replicas = 1
@@ -17,14 +23,30 @@ resource "helm_release" "cockroachdb" {
 		conf = {
 			single-node = true
 		}
-		tls = {
-			enabled = false
-		}
 		storage = {
 			persistentVolume = {
 				storageClass = var.k8s_storage_class
 			}
 		}
+		init = {
+			provisioning = {
+				users = [
+					{
+						user = "rivet-root"
+						password = random_password.root_password.result
+					}
+				]
+			}
+		}
 	})]
+}
+
+data "kubernetes_config_map" "root_ca" {
+	depends_on = [helm_release.cockroachdb]
+
+	metadata {
+		name = "kube-root-ca.crt"
+		namespace = kubernetes_namespace.cockroachdb.metadata.0.name
+	}
 }
 
---
+++ infra/tf/cockroachdb_k8s/outputs.tf
@@ -0,0 +1,21 @@
+output "host"{
+	value = "cockroachdb.cockroachdb.svc.cluster.local"
+}
+
+output "port"{
+	value = 26257
+}
+
+output "cluster_ca_crt"{
+	value = data.kubernetes_config_map.root_ca.data["ca.crt"]
+}
+
+output "username" {
+	value = "rivet-root"
+}
+
+output "password" {
+	value = random_password.root_password.result
+	sensitive = true
+}
+
