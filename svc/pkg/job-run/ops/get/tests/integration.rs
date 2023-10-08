8a46d6643cb07649aac210a112f288769b3f5249
Nathan Flurry <git@nathanflurry.com>
2023-10-08 02:29:19 +0000

    Remvoe ping from job-run-get

--- svc/pkg/job-run/ops/get/tests/integration.rs
+++ svc/pkg/job-run/ops/get/tests/integration.rs
@@ -1,13 +1,11 @@
 use chirp_worker::prelude::*;
-use proto::backend;
+use proto::backend::{self, pkg::*};
 
 #[worker_test]
 async fn empty(ctx: TestCtx) {
 	if !util::feature::job_run() {
 		return;
 	}
-
-	let nomad_config = nomad_util::config_from_env().unwrap();
 
 	let run_res = op!([ctx] faker_job_run {
 		..Default::default()
@@ -23,37 +21,4 @@ async fn empty(ctx: TestCtx) {
 	.await
 	.unwrap();
 	assert_eq!(1, res.runs.len());
-
-	let run = res.runs.first().unwrap();
-	let port = run
-		.ports
-		.iter()
-		.find(|p| p.label == "http")
-		.expect("missing port");
-	let run_meta = match run.run_meta.as_ref().unwrap().kind.as_ref().unwrap() {
-		backend::job::run_meta::Kind::Nomad(x) => x.clone(),
-	};
-
-	// Access the server to validate the test ID from the meta
-	let test_server_addr = format!("http://{}:{}", port.ip, port.source);
-	loop {
-		tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-
-		tracing::info!(%test_server_addr, "fetching test id from job server");
-		let curl_cmd = tokio::process::Command::new("curl")
-			.arg(&test_server_addr)
-			.output()
-			.await
-			.unwrap();
-		if curl_cmd.status.code() == Some(0) {
-			let body = String::from_utf8(curl_cmd.stdout).unwrap();
-			tracing::info!(%body, "successful curl");
-
-			assert_eq!(run_res.test_server_response, body, "wrong server response");
-
-			break;
-		} else {
-			tracing::info!("server not ready yet");
-		}
-	}
 }
