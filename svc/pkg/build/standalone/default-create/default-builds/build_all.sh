11f6673a2b40b2746552cdae55bade15096827b7
Nathan Flurry <git@nathanflurry.com>
2023-10-09 18:53:11 +0000

    Update origins on default build tests

--- svc/pkg/build/standalone/default-create/default-builds/build_all.sh
+++ svc/pkg/build/standalone/default-create/default-builds/build_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -euf -o pipefail
+set -euf
 
 tag=$(date -u +%s)
 
--- svc/pkg/build/standalone/default-create/default-builds/dockerfiles/test-mm-lobby-ready/run.sh
+++ svc/pkg/build/standalone/default-create/default-builds/dockerfiles/test-mm-lobby-ready/run.sh
@@ -11,7 +11,7 @@ cat /etc/resolv.conf
 
 READY_URL="$RIVET_API_URL/matchmaker/lobbies/ready"
 echo "Sending ready to $READY_URL"
-curl --fail --insecure --request POST --header "Content-Type: application/json" --header "Authorization: Bearer $RIVET_TOKEN" --data "{}" "$READY_URL"
+curl --verbose --fail --insecure --request POST --header "Content-Type: application/json" --header "Authorization: Bearer $RIVET_TOKEN" --data "{}" "$READY_URL"
 
 echo "Success, waiting indefinitely"
 tail -f /dev/null
