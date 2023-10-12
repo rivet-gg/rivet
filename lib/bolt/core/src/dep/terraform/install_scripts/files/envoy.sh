fb614681f4a4c393e287975a296dee74a1779d9f
Nathan Flurry <git@nathanflurry.com>
2023-10-12 03:58:14 +0000

    Impl hot restarter for Envoy

--- lib/bolt/core/src/dep/terraform/install_scripts/files/envoy.sh
+++ lib/bolt/core/src/dep/terraform/install_scripts/files/envoy.sh
@@ -5,3 +5,9 @@ mkdir -p "/opt/envoy-${version}"
 curl -L "https://github.com/envoyproxy/envoy/releases/download/v${version}/envoy-${version}-linux-x86_64" -o "/tmp/envoy_${version}"
 install "/tmp/envoy_${version}" /usr/bin/envoy
 
+# Install hot-restarter.py
+#
+# See https://www.envoyproxy.io/docs/envoy/latest/operations/hot_restarter.html
+curl -L "https://raw.githubusercontent.com/envoyproxy/envoy/v${version}/restarter/hot-restarter.py" -o "/tmp/envoy_hot_restarter_${version}.py"
+install "/tmp/envoy_hot_restarter_${version}.py" /usr/bin/envoy_hot_restarter.py
+
--- lib/bolt/core/src/dep/terraform/install_scripts/files/outbound_proxy.sh
+++ lib/bolt/core/src/dep/terraform/install_scripts/files/outbound_proxy.sh
@@ -3,7 +3,7 @@ if ! id -u "outbound_proxy" &>/dev/null; then
         useradd -r -s /bin/false outbound_proxy
 fi
 
-# Write config
+# Config
 mkdir -p /etc/outbound_proxy
 
 cat << 'EOF' > /etc/outbound_proxy/envoy.yaml
@@ -11,6 +11,30 @@ __ENVOY_CONFIG__
 EOF
 
 chown -R outbound_proxy:outbound_proxy /etc/outbound_proxy
+
+# Startup scripts
+#
+# See https://www.envoyproxy.io/docs/envoy/latest/operations/hot_restarter.html
+cat << 'EOF' > /etc/outbound_proxy/start_envoy.sh
+#!/bin/bash
+set -e
+exec /usr/bin/envoy -c /etc/outbound_proxy/envoy.yaml --restart-epoch $RESTART_EPOCH
+EOF
+
+cat << 'EOF' > /etc/outbound_proxy/check_envoy.sh
+#!/bin/sh
+set -e
+/usr/bin/envoy -c /etc/outbound_proxy/envoy.yaml --mode validate
+EOF
+
+cat << 'EOF' > /etc/outbound_proxy/reload_envoy.sh
+#!/bin/sh
+set -e
+/usr/bin/envoy -c /etc/outbound_proxy/envoy.yaml --mode validate
+kill -1 $1
+EOF
+
+chmod +x /etc/outbound_proxy/start_envoy.sh /etc/outbound_proxy/check_envoy.sh /etc/outbound_proxy/reload_envoy.sh
 
 # Systemd service
 cat << 'EOF' > /etc/systemd/system/outbound_proxy.service
@@ -22,8 +46,12 @@ After=network.target
 User=outbound_proxy
 Group=outbound_proxy
 Restart=always
-ExecStart=/usr/bin/envoy -c /etc/outbound_proxy/envoy.yaml
-LimitNOFILE=640000
+ExecStart=/usr/bin/envoy_hot_restarter.py /etc/outbound_proxy/start_envoy.sh
+ExecStartPre=/etc/outbound_proxy/check_envoy.sh
+ExecReload=/etc/outbound_proxy/reload_envoy.sh $MAINPID
+LimitNOFILE=102400
+TimeoutStopSec=10
+KillMode=process
 
 [Install]
 WantedBy=multi-user.target
@@ -33,4 +61,7 @@ EOF
 systemctl daemon-reload
 systemctl enable outbound_proxy
 systemctl start outbound_proxy
+
+# Reload config if already running
+systemctl reload outbound_proxy
 
