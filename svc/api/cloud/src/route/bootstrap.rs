d734bc2f2134403c63f9e6f3429d8524569ab4a0
Nathan Flurry <git@nathanflurry.com>
2023-11-01 05:46:44 +0000

    Add bootstrap endpoint

---
+++ svc/api/cloud/src/route/bootstrap.rs
@@ -0,0 +1,19 @@
+use api_helper::{anchor::WatchIndexQuery, ctx::Ctx};
+use rivet_api::models;
+use rivet_operation::prelude::*;
+
+use crate::auth::Auth;
+
+// MARK: GET /bootstrap
+pub async fn get(
+	_ctx: Ctx<Auth>,
+	_watch_index_query: WatchIndexQuery,
+) -> GlobalResult<models::CloudBootstrapResponse> {
+	Ok(models::CloudBootstrapResponse {
+		captcha: Box::new(models::CloudBootstrapCaptcha {
+			turnstile: Box::new(models::CloudBootstrapCaptchaTurnstile {
+				site_key: std::env::var("TURNSTILE_SITE_KEY_MAIN")?,
+			}),
+		}),
+	})
+}
