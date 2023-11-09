6e3e848b772e5eb2a16cfb51d438e6e13e0b83f7
MasterPtato <maxpopkov.contact@gmail.com>
2023-11-09 23:18:39 +0000

    Fix monolith metrics

--- svc/pkg/monolith/standalone/worker/src/main.rs
+++ svc/pkg/monolith/standalone/worker/src/main.rs
@@ -5,7 +5,21 @@ fn main() -> GlobalResult<()> {
 }
 
 async fn start() -> GlobalResult<()> {
-	monolith_worker::run_from_env().await?;
+	let pools = rivet_pools::from_env("monolith-worker").await?;
+
+	tokio::task::Builder::new()
+		.name("monolith_worker::health_checks")
+		.spawn(rivet_health_checks::run_standalone(
+			rivet_health_checks::Config {
+				pools: Some(pools.clone()),
+			},
+		))?;
+
+	tokio::task::Builder::new()
+		.name("monolith_worker::metrics")
+		.spawn(rivet_metrics::run_standalone())?;
+		
+	monolith_worker::run_from_env(pools).await?;
 
 	Ok(())
 }
