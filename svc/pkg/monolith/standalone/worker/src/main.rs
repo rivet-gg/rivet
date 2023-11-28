70330256aa818a6da6868ebeb7ea09e0c5ae7a0c
Forest Anderson <forestkzanderson@gmail.com>
2023-11-28 17:38:15 -0500

    Remove duplicated health check and metrics services

--- svc/pkg/monolith/standalone/worker/src/main.rs
+++ svc/pkg/monolith/standalone/worker/src/main.rs
@@ -7,18 +7,6 @@ fn main() -> GlobalResult<()> {
 async fn start() -> GlobalResult<()> {
 	let pools = rivet_pools::from_env("monolith-worker").await?;
 
-	tokio::task::Builder::new()
-		.name("monolith_worker::health_checks")
-		.spawn(rivet_health_checks::run_standalone(
-			rivet_health_checks::Config {
-				pools: Some(pools.clone()),
-			},
-		))?;
-
-	tokio::task::Builder::new()
-		.name("monolith_worker::metrics")
-		.spawn(rivet_metrics::run_standalone())?;
-
 	monolith_worker::run_from_env(pools).await?;
 
 	Ok(())
