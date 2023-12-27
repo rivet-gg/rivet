42ed85776235c5e51107d28d696ddd2a4ec5a675
MasterPtato <maxpopkov.contact@gmail.com>
2023-12-27 00:32:44 +0000

    FIx hook script

--- svc/pkg/cluster/worker/src/workers/server_install/install_scripts/files/rivet_fetch_info.sh
+++ svc/pkg/cluster/worker/src/workers/server_install/install_scripts/files/rivet_fetch_info.sh
@@ -8,17 +8,19 @@ response=$(
 )
 
 # Fetch data
-var1=$(echo $response | jq '.field1')
-var2=$(echo $response | jq '.field2')
-var3=$(echo $response | jq '.nestedField.field3')
+name=$(echo $response | jq -r '.name')
+server_id=$(echo $response | jq -r '.server_id')
+datacenter_id=$(echo $response | jq -r '.datacenter_id')
+cluster_id=$(echo $response | jq -r '.cluster_id')
+vlan_ip=$(echo $response | jq -r '.vlan_ip')
 
 # Template initialize script
 initialize_script="/usr/bin/rivet_initialize.sh"
-sed -i "s/__NODE_NAME__/$var1/g" $initialize_script
-sed -i "s/__SERVER_ID__/$var1/g" $initialize_script
-sed -i "s/__DATACENTER_ID__/$var1/g" $initialize_script
-sed -i "s/__CLUSTER_ID__/$var1/g" $initialize_script
-sed -i "s/__VLAN_IP__/$var1/g" $initialize_script
+sed -i "s/__NODE_NAME__/$name/g" $initialize_script
+sed -i "s/__SERVER_ID__/$server_id/g" $initialize_script
+sed -i "s/__DATACENTER_ID__/$datacenter_id/g" $initialize_script
+sed -i "s/__CLUSTER_ID__/$cluster_id/g" $initialize_script
+sed -i "s/__VLAN_IP__/$vlan_ip/g" $initialize_script
 
 # Run initialize script
 "$initialize_script"
