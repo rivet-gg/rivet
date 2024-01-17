e3230e24e8d4ea10cc7352ce1ba3f3d48c3271d2
Nathan Flurry <git@nathanflurry.com>
2024-01-17 10:28:14 +0000

    Update forward Treafik scripts (#355)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- scripts/forward/traefik_tunnel.sh
+++ scripts/forward/traefik_tunnel.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -euf
 
-echo 'Dashboard: http://localhost:9090/dashboard/'
-echo 'API: http://localhost:9090/api/'
+# We don't know the value of $PORT yet
+echo 'Dashboard: http://localhost:$PORT/dashboard/'
+echo 'API: http://localhost:$PORT/api/'
 
 FORWARD_NS=traefik-tunnel FORWARD_NAME=service/traefik-headless FORWARD_PORT=9000 ./scripts/forward/service.sh
 
