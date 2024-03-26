f1546c69342216a985c0845b8c8592e09f3ecc15
NathanFlurry <git@nathanflurry.com>
2024-03-26 17:03:42 +0000

    fix: remove hardcoded eks role (#586)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- infra/tf/k8s_cluster_aws/providers.tf
+++ infra/tf/k8s_cluster_aws/providers.tf
@@ -18,7 +18,7 @@ provider "kubernetes" {
 	exec {
 		api_version = "client.authentication.k8s.io/v1beta1"
 		command = "aws"
-		args = ["--region", "us-east-1", "eks", "get-token", "--cluster-name", module.eks.cluster_name, "--output", "json", "--role", "arn:aws:iam::717589162638:role/rivet-staging2-Admin"]
+		args = ["--region", "us-east-1", "eks", "get-token", "--cluster-name", module.eks.cluster_name, "--output", "json"]
 	}
 }
 
@@ -30,7 +30,7 @@ provider "helm" {
 		exec {
 			api_version = "client.authentication.k8s.io/v1beta1"
 			command = "aws"
-			args = ["--region", "us-east-1", "eks", "get-token", "--cluster-name", module.eks.cluster_name, "--output", "json", "--role", "arn:aws:iam::717589162638:role/rivet-staging2-Admin"]
+			args = ["--region", "us-east-1", "eks", "get-token", "--cluster-name", module.eks.cluster_name, "--output", "json"]
 		}
 	}
 }
@@ -44,6 +44,6 @@ provider "kubectl" {
 	exec {
 		api_version = "client.authentication.k8s.io/v1beta1"
 		command = "aws"
-		args = ["--region", "us-east-1", "eks", "get-token", "--cluster-name", module.eks.cluster_name, "--output", "json", "--role", "arn:aws:iam::717589162638:role/rivet-staging2-Admin"]
+		args = ["--region", "us-east-1", "eks", "get-token", "--cluster-name", module.eks.cluster_name, "--output", "json"]
 	}
 }
