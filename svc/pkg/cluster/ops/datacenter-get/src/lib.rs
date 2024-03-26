4755abbb9519ad667fdb7d8c68c7ed2f6b6b6334
MasterPtato <maxpopkov.contact@gmail.com>
2024-03-26 20:50:45 +0000

    fix: restructure datacenter table

--- svc/pkg/cluster/ops/datacenter-get/src/lib.rs
+++ svc/pkg/cluster/ops/datacenter-get/src/lib.rs
@@ -1,5 +1,40 @@
+use std::convert::{TryFrom, TryInto};
+
 use proto::backend::{self, pkg::*};
 use rivet_operation::prelude::*;
+
+#[derive(sqlx::FromRow)]
+struct Datacenter {
+	datacenter_id: Uuid,
+	cluster_id: Uuid,
+	name_id: String,
+	display_name: String,
+	provider: i64,
+	provider_datacenter_id: String,
+	pools: Vec<u8>,
+	build_delivery_method: i64,
+	drain_timeout: i64,
+}
+
+impl TryFrom<Datacenter> for backend::cluster::Datacenter {
+	type Error = GlobalError;
+
+	fn try_from(value: Datacenter) -> GlobalResult<Self> {
+		let pools = cluster::msg::datacenter_create::Pools::decode(value.pools.as_slice())?.pools;
+
+		Ok(backend::cluster::Datacenter {
+			datacenter_id: Some(value.datacenter_id.into()),
+			cluster_id: Some(value.cluster_id.into()),
+			name_id: value.name_id,
+			display_name: value.display_name,
+			provider: value.provider as i32,
+			provider_datacenter_id: value.provider_datacenter_id,
+			pools,
+			build_delivery_method: value.build_delivery_method as i32,
+			drain_timeout: value.drain_timeout as u64,
+		})
+	}
+}
 
 #[operation(name = "cluster-datacenter-get")]
 pub async fn handle(
@@ -12,23 +47,30 @@ pub async fn handle(
 		.collect::<Vec<_>>();
 
 	let configs = sql_fetch_all!(
-		[ctx, (Vec<u8>,)]
+		[ctx, Datacenter]
 		"
 		SELECT
-			config
+			datacenter_id,
+			cluster_id,
+			name_id,
+			display_name,
+			provider,
+			provider_datacenter_id,
+			provider_api_token,
+			pools,
+			build_delivery_method,
+			drain_timeout
 		FROM db_cluster.datacenters
 		WHERE datacenter_id = ANY($1)
 		",
-		datacenter_ids
+		datacenter_ids,
 	)
 	.await?;
 
 	Ok(cluster::datacenter_get::Response {
 		datacenters: configs
 			.into_iter()
-			.map(|(config_bytes,)| {
-				backend::cluster::Datacenter::decode(config_bytes.as_slice()).map_err(Into::into)
-			})
+			.map(TryInto::try_into)
 			.collect::<GlobalResult<Vec<_>>>()?,
 	})
 }
