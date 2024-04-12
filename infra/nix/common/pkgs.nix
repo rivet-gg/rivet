91792d07d77fde11a285416536d6c058ef56d882
NathanFlurry <git@nathanflurry.com>
2024-04-12 07:26:38 +0000

    fix: update rust nix pkg (#648)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- infra/nix/common/pkgs.nix
+++ infra/nix/common/pkgs.nix
@@ -1,6 +1,6 @@
 let
 	# Pull new Rust packages
-	moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/e6ca26fe8b9df914d4567604e426fbc185d9ef3e.tar.gz);
+	moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/9b11a87c0cc54e308fa83aac5b4ee1816d5418a2.tar.gz);
 
 	# Overlay package
 	pkgs = import (fetchTarball {
