4b5034e17b3ffbddcfcc12300383dad2e2ca7a25
ABC <79597906+ABCxFF@users.noreply.github.com>
2024-07-03 15:27:55 -0400

    fix(sdks/runtime): RVT-3805 / #982

--- sdks/runtime/typescript/src/core/fetcher/Fetcher.ts
+++ sdks/runtime/typescript/src/core/fetcher/Fetcher.ts
@@ -73,15 +73,15 @@ async function fetcherImpl<R = unknown>(args: Fetcher.Args): Promise<APIResponse
         body = JSON.stringify(args.body);
     }
 
-    const fetchFn = typeof fetch == "function" ? fetch : require("node-fetch");
-
     const makeRequest = async (): Promise<Response> => {
         const controller = new AbortController();
         let abortId = undefined;
         if (args.timeoutMs != null) {
             abortId = setTimeout(() => controller.abort(), args.timeoutMs);
         }
-        const response = await fetchFn(url, {
+
+		// We assume here that the environment is providing fetch functionality
+        const response = await fetch(url, {
             method: args.method,
             headers,
             body,
