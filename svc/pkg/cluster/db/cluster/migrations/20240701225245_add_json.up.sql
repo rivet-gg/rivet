4987029cab47ed7f5f903b5ade1c4c03cae3ee7b
NathanFlurry <git@nathanflurry.com>
2024-08-07 00:24:43 +0000

    fix(cluster): split up backfill query from schema change (#1023)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- svc/pkg/cluster/db/cluster/migrations/20240701225245_add_json.up.sql
+++ svc/pkg/cluster/db/cluster/migrations/20240701225245_add_json.up.sql
@@ -26,27 +26,3 @@ CREATE TABLE server_images2 (
 	PRIMARY KEY (provider, install_hash, datacenter_id, pool_type)
 );
 
--- Backfill
-UPDATE datacenters
-SET provider2 = '"Linode"'
-WHERE provider = 0;
-
-UPDATE servers
-SET pool_type2 = '"Job"'
-WHERE pool_type = 0;
-UPDATE servers
-SET pool_type2 = '"Gg"'
-WHERE pool_type = 1;
-UPDATE servers
-SET pool_type2 = '"Ats"'
-WHERE pool_type = 2;
-
-UPDATE datacenter_tls
-SET state2 = '"Creating"'
-WHERE state = 0;
-UPDATE datacenter_tls
-SET state2 = '"Active"'
-WHERE state = 1;
-UPDATE datacenter_tls
-SET state2 = '"Renewing"'
-WHERE state = 2;
