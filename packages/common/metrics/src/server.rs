0b90d900946c36b1908d5e274f40a80a1b545d98
Nathan Flurry <developer@nathanflurry.com>
2024-10-28 21:57:29 -0700

    chore: fail fast on service initialization

--- packages/common/metrics/src/server.rs
+++ packages/common/metrics/src/server.rs
@@ -1,26 +1,32 @@
-use std::net::SocketAddr;
-
+use global_error::prelude::*;
 use hyper::{
 	header::CONTENT_TYPE,
 	service::{make_service_fn, service_fn},
 	Body, Request, Response, Server,
 };
 use prometheus::{Encoder, TextEncoder};
+use std::net::SocketAddr;
 
 // TODO: Record extra labels
 
 #[tracing::instrument(skip_all)]
-pub async fn run_standalone(config: rivet_config::Config) {
-	let host = config.server().unwrap().rivet.metrics.host();
-	let port = config.server().unwrap().rivet.metrics.port();
+pub fn spawn_standalone(config: rivet_config::Config) -> GlobalResult<()> {
+	let host = config.server()?.rivet.metrics.host();
+	let port = config.server()?.rivet.metrics.port();
 	let addr = SocketAddr::from((host, port));
 
-	let server = Server::bind(&addr).serve(make_service_fn(|_| async {
+	let server = Server::try_bind(&addr)?.serve(make_service_fn(|_| async {
 		Ok::<_, hyper::Error>(service_fn(serve_req))
 	}));
-	if let Err(err) = server.await {
-		tracing::error!(?err, "metrics server error");
-	}
+
+	tokio::spawn(async move {
+		tracing::info!(?host, ?port, "started metrics server");
+		if let Err(err) = server.await {
+			tracing::error!(?err, "metrics server error");
+		}
+	});
+
+	Ok(())
 }
 
 #[tracing::instrument(skip_all)]
