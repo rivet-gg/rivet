e6021cca636f74eb700377d1d4d1a1d7737a7441
NathanFlurry <git@nathanflurry.com>
2024-11-14 05:21:09 +0000

    chore: fix dev clickhouse migrations & storage engine (#1350)
    Fixes RVT-4061

--- packages/common/config/src/config/server/mod.rs
+++ packages/common/config/src/config/server/mod.rs
@@ -403,7 +403,10 @@ impl Default for Nats {
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "snake_case", deny_unknown_fields)]
 pub struct ClickHouse {
-	pub url: Url,
+	/// URL to the HTTP access port for ClickHouse.
+	pub http_url: Url,
+	/// URL to the native access port for ClickHouse.
+	pub native_url: Url,
 	pub username: String,
 	#[serde(default)]
 	pub password: Option<Secret<String>>,
