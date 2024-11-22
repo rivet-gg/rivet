44d9f7c8de4bdaddaea6a233400c93df12c271e4
MasterPtato <23087326+MasterPtato@users.noreply.github.com>
2024-11-22 03:47:27 +0000

    fix: make kv metadata version to bytes (#1435)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- packages/infra/client/actor-kv/src/metadata.rs
+++ packages/infra/client/actor-kv/src/metadata.rs
@@ -2,8 +2,8 @@ use serde::Serialize;
 
 #[derive(Clone, PartialEq, ::prost::Message, Serialize)]
 pub struct Metadata {
-	#[prost(string, tag = "1")]
-	pub kv_version: ::prost::alloc::string::String,
+	#[prost(bytes = "vec", tag = "1")]
+	pub kv_version: Vec<u8>,
 	#[prost(int64, tag = "2")]
 	pub create_ts: i64,
 }
