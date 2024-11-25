8be6328e1eb716fa0aac32e0f1b60ef9eff59008
Nathan Flurry <developer@nathanflurry.com>
2024-11-24 16:19:55 -0800

    fix: update default hub origin to have /ui/ path

--- packages/common/hub-embed/build.rs
+++ packages/common/hub-embed/build.rs
@@ -1,6 +1,6 @@
 use std::{env, fs, path::Path};
 
-const HUB_URL: &str = "https://releases.rivet.gg/hub/2024-11-13-06-23-39-b01b441-embed.zip";
+const HUB_URL: &str = "https://releases.rivet.gg/hub/2024-11-25-00-13-42-34b1869-embed.zip";
 const OUT_DIR: &str = "hub_files";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
