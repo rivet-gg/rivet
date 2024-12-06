1bbea752489892dea2ca0fcbe8d8f9e5ebcaaafe
MasterPtato <maxpopkov.contact@gmail.com>
2024-12-06 01:51:54 +0000

    feat: add region proxying to actor client

---
+++ sdks/api/runtime/rust/docs/ActorResolveRegionResponse.md
@@ -0,0 +1,11 @@
+# ActorResolveRegionResponse
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**region** | [**crate::models::ActorRegion**](ActorRegion.md) |  | 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
--- sdks/api/runtime/rust/src/apis/actor_regions_api.rs
+++ sdks/api/runtime/rust/src/apis/actor_regions_api.rs
@@ -28,6 +28,19 @@ pub enum ActorRegionsListError {
     UnknownValue(serde_json::Value),
 }
 
+/// struct for typed errors of method [`actor_regions_resolve`]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ActorRegionsResolveError {
+    Status400(crate::models::ErrorBody),
+    Status403(crate::models::ErrorBody),
+    Status404(crate::models::ErrorBody),
+    Status408(crate::models::ErrorBody),
+    Status429(crate::models::ErrorBody),
+    Status500(crate::models::ErrorBody),
+    UnknownValue(serde_json::Value),
+}
+
 
 pub async fn actor_regions_list(configuration: &configuration::Configuration, project: Option<&str>, environment: Option<&str>) -> Result<crate::models::ActorListRegionsResponse, Error<ActorRegionsListError>> {
     let local_var_configuration = configuration;
@@ -60,6 +73,42 @@ pub async fn actor_regions_list(configuration: &configuration::Configuration, pr
         serde_json::from_str(&local_var_content).map_err(Error::from)
     } else {
         let local_var_entity: Option<ActorRegionsListError> = serde_json::from_str(&local_var_content).ok();
+        let local_var_error = ResponseContent { status: local_var_status, content: local_var_content, entity: local_var_entity };
+        Err(Error::ResponseError(local_var_error))
+    }
+}
+
+pub async fn actor_regions_resolve(configuration: &configuration::Configuration, long: Option<f64>, lat: Option<f64>) -> Result<crate::models::ActorResolveRegionResponse, Error<ActorRegionsResolveError>> {
+    let local_var_configuration = configuration;
+
+    let local_var_client = &local_var_configuration.client;
+
+    let local_var_uri_str = format!("{}/regions/resolve", local_var_configuration.base_path);
+    let mut local_var_req_builder = local_var_client.request(reqwest::Method::GET, local_var_uri_str.as_str());
+
+    if let Some(ref local_var_str) = long {
+        local_var_req_builder = local_var_req_builder.query(&[("long", &local_var_str.to_string())]);
+    }
+    if let Some(ref local_var_str) = lat {
+        local_var_req_builder = local_var_req_builder.query(&[("lat", &local_var_str.to_string())]);
+    }
+    if let Some(ref local_var_user_agent) = local_var_configuration.user_agent {
+        local_var_req_builder = local_var_req_builder.header(reqwest::header::USER_AGENT, local_var_user_agent.clone());
+    }
+    if let Some(ref local_var_token) = local_var_configuration.bearer_access_token {
+        local_var_req_builder = local_var_req_builder.bearer_auth(local_var_token.to_owned());
+    };
+
+    let local_var_req = local_var_req_builder.build()?;
+    let local_var_resp = local_var_client.execute(local_var_req).await?;
+
+    let local_var_status = local_var_resp.status();
+    let local_var_content = local_var_resp.text().await?;
+
+    if !local_var_status.is_client_error() && !local_var_status.is_server_error() {
+        serde_json::from_str(&local_var_content).map_err(Error::from)
+    } else {
+        let local_var_entity: Option<ActorRegionsResolveError> = serde_json::from_str(&local_var_content).ok();
         let local_var_error = ResponseContent { status: local_var_status, content: local_var_content, entity: local_var_entity };
         Err(Error::ResponseError(local_var_error))
     }
