c8dfaedc5a627c11228f7afca49236fbee49ad3c
NathanFlurry <git@nathanflurry.com>
2024-12-11 19:51:44 +0000

    chore: update init projects (#1591)
    Fixes RVT-4357

---
+++ examples/init-template-js/counter_test.js
@@ -0,0 +1,16 @@
+import { TestClient } from "@rivet-gg/actors-client/test";
+
+const client = new TestClient();
+
+// Get-or-create a counter actor
+const counter = await client.get({ name: "counter" });
+
+// Listen for update count events (https://rivet.gg/docs/events)
+counter.on("count", (count) => console.log("New count:", count));
+
+// Increment the count over remote procedure call (https://rivet.gg/docs/rpc)
+await counter.increment(1);
+
+// Disconnect from the actor when finished (https://rivet.gg/docs/connections)
+counter.disconnect();
+
