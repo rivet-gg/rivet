91820700a1bc4f4415faf8c21cc3739007689cd9
Nathan Flurry <developer@nathanflurry.com>
2024-12-11 16:48:13 -0800

    chore: expose logging to actors

--- examples/counter/counter.ts
+++ examples/counter/counter.ts
@@ -13,13 +13,17 @@ interface ConnParams {
 	mod: number;
 }
 
-export default class Counter extends Actor<State, ConnParams | undefined, ConnState> {
+export default class Counter extends Actor<
+	State,
+	ConnParams | undefined,
+	ConnState
+> {
 	override _onInitialize(): State {
 		return { count: 0 };
 	}
 
 	override _onBeforeConnect(opts: OnBeforeConnectOpts<ConnParams>): ConnState {
-		console.log("parameters", opts.parameters);
+		this.log.info("parameters", { params: opts.parameters });
 		return { mod: opts.parameters?.mod ?? 1 };
 	}
 
@@ -27,8 +31,8 @@ export default class Counter extends Actor<State, ConnParams | undefined, ConnSt
 		this._broadcast("broadcastCount", newState.count);
 
 		for (const conn of this.connections.values()) {
-			console.log("checking mod", conn.id, conn.state);
-			console.log("state", conn.state);
+			this.log.info("checking mod", { id: conn.id, state: conn.state });
+			this.log.info("state", { state: conn.state });
 			if (newState.count % conn.state.mod === 0) {
 				conn.send("directCount", newState.count);
 			}
@@ -36,6 +40,7 @@ export default class Counter extends Actor<State, ConnParams | undefined, ConnSt
 	}
 
 	increment(_rpc: Rpc<Counter>, count: number): number {
+		this.log.info("increment", { count });
 		this.state.count += count;
 		return this.state.count;
 	}
--- sdks/actors/runtime/src/log.ts
+++ sdks/actors/runtime/src/log.ts
@@ -1,7 +1,15 @@
 import { getLogger } from "../../common/src/log.ts";
 
+/** Logger for this library. */
 export const LOGGER_NAME = "actors";
+
+/** Logger used for logs from the actor instance itself. */
+export const INSTANCE_LOGGER_NAME = "actors-instance";
 
 export function logger() {
 	return getLogger(LOGGER_NAME);
+}
+
+export function instanceLogger() {
+	return getLogger(INSTANCE_LOGGER_NAME);
 }
