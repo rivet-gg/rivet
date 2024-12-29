d3ed8a077b380e61f6fadca5bf5ff04a30371590
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2024-12-29 22:23:08 +0100

    fix(site): pricing page anchors to bottom of page

--- site/src/app/(v2)/(framer)/page.tsx
+++ site/src/app/(v2)/(framer)/page.tsx
@@ -7,8 +7,11 @@ export const metadata: Metadata = {
 };
 
 export default function IndexPage() {
+  // an empty div at the top of the page is a workaround for a bug in Next.js that causes the page to jump when the user navigates to it
+  // https://github.com/vercel/next.js/discussions/64534
   return (
     <>
+      <div />
       <FancyHeader />
       <FramerIndexPage />
     </>
--- site/src/app/(v2)/(framer)/pricing/page.tsx
+++ site/src/app/(v2)/(framer)/pricing/page.tsx
@@ -2,8 +2,11 @@ import { FramerPricingPage } from '@/components/framer/PricingPage';
 import { FancyHeader } from '@/components/v2/FancyHeader';
 
 export default function PricingPage() {
+  // an empty div at the top of the page is a workaround for a bug in Next.js that causes the page to jump when the user navigates to it
+  // https://github.com/vercel/next.js/discussions/64534
   return (
     <>
+      <div />
       <FancyHeader active='pricing' />
       <FramerPricingPage />
     </>
