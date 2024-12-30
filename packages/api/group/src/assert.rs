a1b13164787bdd7b2c91a618c2783cee34867470
ABCxFF <79597906+ABCxFF@users.noreply.github.com>
2024-12-30 22:58:47 +0000

    chore(users): migrate user-identity ops to user

--- packages/api/group/src/assert.rs
+++ packages/api/group/src/assert.rs
@@ -6,8 +6,8 @@ use crate::auth::Auth;
 /// Validates that a given user ID is registered.
 pub async fn user_registered(ctx: &Ctx<Auth>, user_id: Uuid) -> GlobalResult<()> {
 	// If the user has at least one identity they are considered registered
-	let identity = op!([ctx] user_identity_get {
-		user_ids: vec![user_id.into()]
+	let identity = (*ctx).op(::user::ops::identity::get::Input {
+		user_ids: vec![user_id]
 	})
 	.await?;
 
