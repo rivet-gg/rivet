3254c8cc1e48564e032a5b41030f4a4da7449510
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2025-01-02 16:55:43 +0100

    chore(examples): add rsc example

--- examples/react/src/app/page.tsx
+++ examples/react/src/app/page.tsx
@@ -1,31 +1,39 @@
 "use client";
 
+import { ServerDrivenUi } from "./server-driven-ui";
 import styles from "./page.module.css";
 import { SimpleChat } from "./simple-chat";
 import { ActorClientProvider } from "./use-actor";
 import { Client } from "@rivet-gg/actor-client";
+import { Suspense } from "react";
 
 const actorClient = new Client(process.env.NEXT_PUBLIC_ACTOR_MANAGER_URL!);
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div>
-          <img src="/logo.svg" alt="Rivet Logo" className={styles.logo} />
-          <h1>Rivet Actors &times; React</h1>
-        </div>
+	return (
+		<div className={styles.page}>
+			<main className={styles.main}>
+				<ActorClientProvider client={actorClient}>
+					<div>
+						<img src="/logo.svg" alt="Rivet Logo" className={styles.logo} />
+						<h1>Rivet Actors &times; React</h1>
+					</div>
 
-        <section>
-          <p>
-            This chat is powered by Rivet Actors and React. Open a new tab to
-            see changes in real-time. Send messages and receive them.
-          </p>
-          <ActorClientProvider client={actorClient}>
-            <SimpleChat />
-          </ActorClientProvider>
-        </section>
-      </main>
-    </div>
-  );
+					<section>
+						<p>
+							This chat is powered by Rivet Actors and React. Open a new tab to see changes in
+							real-time. Send messages and receive them.
+						</p>
+						<Suspense>
+							<SimpleChat />
+						</Suspense>
+					</section>
+					<section>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus</p>
+						<ServerDrivenUi />
+					</section>
+				</ActorClientProvider>
+			</main>
+		</div>
+	);
 }
--- examples/react/src/app/use-actor.tsx
+++ examples/react/src/app/use-actor.tsx
@@ -1,72 +1,267 @@
 "use client";
-import { ActorHandle, Client } from "@rivet-gg/actor-client";
-import { createContext, use, useEffect, useState } from "react";
+import { Client as ActorClient, ActorHandle } from "@rivet-gg/actor-client";
+import {
+	createContext,
+	ElementType,
+	use,
+	useCallback,
+	useEffect,
+	useMemo,
+	useRef,
+	useState,
+	useSyncExternalStore,
+} from "react";
+// @ts-ignore
+import { createFromFetch } from "@kentcdodds/tmp-react-server-dom-esm/client.browser";
 
-const ActorClientContext = createContext<Client | null>(null);
+export const ActorClientContext = createContext<ActorClient | null>(null);
 
 export function ActorClientProvider({
-  client,
-  children,
+	client,
+	children,
 }: {
-  children: React.ReactNode;
-  client: Client;
+	children: React.ReactNode;
+	client: ActorClient;
 }) {
-  return (
-    <ActorClientContext.Provider value={client}>
-      {children}
-    </ActorClientContext.Provider>
-  );
+	return <ActorClientContext.Provider value={client}>{children}</ActorClientContext.Provider>;
 }
 
-type ActorState<T> =
-  | { isLoading: true }
-  | { isLoading: false }
-  | { isLoading: false; actor: ActorHandle<T> }
-  | { isLoading: false; error: unknown };
+export function useActorClient() {
+	const manager = use(ActorClientContext);
+	if (!manager) {
+		throw new Error("useActorClient must be used within an ActorClientProvider");
+	}
+	return manager;
+}
 
-export function useActor<T>(...params: Parameters<Client["get"]>) {
-  const manager = use(ActorClientContext);
-  if (!manager) {
-    throw new Error("useActor must be used within an ActorManager");
-  }
+const noop = () => {
+	// noop
+};
 
-  const [state, setState] = useState<ActorState<T>>({ isLoading: false });
+/**
+ * Shallow compare objects.
+ * Copied from https://github.com/TanStack/query/blob/3c5d8e348cc53e46aea6c74767f3181fc77c2308/packages/query-core/src/utils.ts#L298-L299
+ */
+export function shallowEqualObjects<T extends Record<string, any>>(
+	a: T | undefined,
+	b: T | undefined
+): boolean {
+	if (a === undefined && b === undefined) {
+		return true;
+	}
+	if (!a || !b || Object.keys(a).length !== Object.keys(b).length) {
+		return false;
+	}
 
-  async function create(...params: Parameters<Client["get"]>) {
-    if (state.isLoading) {
-      return;
-    }
-    dispose();
-    setState({ isLoading: true });
-    try {
-      const actor = await manager?.get<T>(...params);
-      if (!actor) {
-        return setState({
-          isLoading: false,
-          error: new Error("Actor not found"),
-        });
-      }
-      setState({ isLoading: false, actor });
-    } catch (e) {
-      setState({ isLoading: false, error: e });
-    }
-  }
+	for (const key in a) {
+		if (a[key] !== b[key]) {
+			if (typeof a[key] === "object" && typeof b[key] === "object") {
+				return shallowEqualObjects(a[key], b[key]);
+			}
+			return false;
+		}
+	}
 
-  function dispose() {
-    if ("actor" in state) {
-      state.actor.dispose();
-      setState({ isLoading: false });
-    }
+	return true;
+}
 
-    setState({ isLoading: false });
-  }
+const ACTOR_HANDLE_STATE_INIT = { isLoading: false, error: null, actor: null } as const;
+const ACTOR_HANDLE_STATE_CREATING = { isLoading: true, error: null, actor: null } as const;
+function ACTOR_HANDLE_STATE_CREATED<A = unknown>(actor: ActorHandle<A>) {
+	return { isLoading: false, error: null, actor } as const;
+}
+function ACTOR_HANDLE_STATE_ERRORED(error: unknown) {
+	return { isLoading: false, error, actor: null } as const;
+}
 
-  useEffect(() => {
-    create(...params);
-    return () => {
-      dispose();
-    };
-  }, []);
+type ActorManagerState<A> =
+	| typeof ACTOR_HANDLE_STATE_INIT
+	| typeof ACTOR_HANDLE_STATE_CREATING
+	| { isLoading: false; error: null; actor: ActorHandle<A> }
+	| { isLoading: false; error: unknown; actor: null };
 
-  return { create, dispose, ...state };
+class ActorManager<A = unknown> {
+	#client: ActorClient;
+	#options: Parameters<ActorClient["get"]>;
+
+	#listeners: (() => void)[] = [];
+
+	#state: ActorManagerState<A> = ACTOR_HANDLE_STATE_INIT;
+
+	#createPromise: Promise<ActorHandle<A>> | null = null;
+
+	constructor(client: ActorClient, options: Parameters<ActorClient["get"]>) {
+		this.#client = client;
+		this.#options = options;
+	}
+
+	setOptions(options: Parameters<ActorClient["get"]>) {
+		if (shallowEqualObjects(options, this.#options)) {
+			if (!this.#state.actor) {
+				this.create();
+			}
+			return;
+		}
+
+		this.#state.actor?.dispose();
+
+		this.#state = ACTOR_HANDLE_STATE_INIT;
+		this.#options = options;
+		this.#update();
+		this.create();
+	}
+
+	async create() {
+		if (this.#createPromise) {
+			return this.#createPromise;
+		}
+		this.#state = { ...ACTOR_HANDLE_STATE_CREATING };
+		this.#update();
+		try {
+			this.#createPromise = this.#client.get<A>(...this.#options);
+			const actor = await this.#createPromise;
+			this.#state = ACTOR_HANDLE_STATE_CREATED(actor);
+			this.#createPromise = null;
+		} catch (e) {
+			this.#state = ACTOR_HANDLE_STATE_ERRORED(e);
+		} finally {
+			this.#update();
+		}
+	}
+
+	getState() {
+		return this.#state;
+	}
+
+	subscribe(cb: () => void) {
+		this.#listeners.push(cb);
+		return () => {
+			this.#listeners = this.#listeners.filter((l) => l !== cb);
+		};
+	}
+
+	#update() {
+		this.#listeners.forEach((cb) => cb());
+	}
+}
+
+function useActorHook<A = unknown>(...options: Parameters<ActorClient["get"]>) {
+	const client = useActorClient();
+
+	const [manager] = useState(() => new ActorManager<A>(client, options));
+
+	const state = useSyncExternalStore(
+		useCallback((onUpdate) => {
+			return manager.subscribe(onUpdate);
+		}, []),
+		() => manager.getState(),
+		() => manager.getState()
+	);
+
+	useEffect(() => {
+		manager.setOptions(options);
+	}, [options, manager]);
+
+	return [state] as const;
+}
+
+function useActorEventHook<A = unknown, Args extends any[] = unknown[]>(
+	opts: { actor: ActorHandle<A> | null; event: string },
+	cb: (...args: Args) => void
+) {
+	const ref = useRef(cb);
+
+	useEffect(() => {
+		ref.current = cb;
+	}, [cb]);
+
+	useEffect(() => {
+		if (!opts.actor) {
+			return noop;
+		}
+		const unsub = opts.actor.on(opts.event, (...args: Args) => {
+			ref.current(...args);
+		});
+
+		return unsub;
+	}, [opts.actor, opts.event]);
+}
+
+function useRscHook<A = unknown>(opts: { actor: ActorHandle<A> | null; fn: keyof ActorHandle<A> }) {
+	const [lastResponse, setLastResponse] = useState<ElementType>(() => () => null);
+
+	const rsc = useCallback(
+		async (...args: any[]) => {
+			if (opts.actor === null) {
+				return () => null;
+			}
+			const response = (await opts.actor[opts.fn](...args)) as string;
+			const data = new Response(response);
+			const jsx = await createFromFetch(Promise.resolve(data));
+			setLastResponse(() => () => jsx);
+
+			return jsx;
+		},
+		[opts.actor, opts.fn]
+	);
+
+	return [lastResponse, rsc] as const;
+}
+
+type UseActorParameters =
+	| [Omit<Parameters<ActorClient["get"]>[0], "name">, Parameters<ActorClient["get"]>[1]]
+	| [Omit<Parameters<ActorClient["get"]>[0], "name">]
+	| [];
+
+export function unstable_createActorHooks<A = unknown>(opts: { name: string }) {
+	return {
+		useActor(...options: UseActorParameters) {
+			const [req, ...rest] = options;
+			return useActorHook<A>({ ...opts, ...req }, ...rest);
+		},
+		useActorEventCallback<E extends string, Args extends any[] = unknown[]>(
+			opts: { actor: ActorHandle<A> | null; event: E },
+			cb: (...args: Args) => void
+		) {
+			return useActorEventHook<A, Args>(opts, cb);
+		},
+		useActorRsc<A = unknown>(opts: { actor: ActorHandle<A> | null; fn: keyof ActorHandle<A> }) {
+			return useRscHook(opts);
+		},
+	};
+}
+
+function ServerComponent<A>({
+	actor,
+	fn,
+	...props
+}: {
+	actor: ActorHandle<A> | null;
+	fn: keyof ActorHandle<A>;
+}) {
+	const [Comp, refetch] = useRscHook({ actor, fn });
+	useActorEventHook<A>({ actor, event: "__rsc" }, () => {
+		refetch(props);
+	});
+	useEffect(() => {
+		refetch(props);
+	}, []);
+
+	return <Comp />;
+}
+
+export function useActor<A = unknown>(...options: Parameters<ActorClient["get"]>) {
+	const [{ actor, ...rest }] = useActorHook<A>(...options);
+
+	const rsc = useMemo(() => {
+		return new Proxy({} as ActorHandle<A>, {
+			get(_target, prop) {
+				return (props: Record<string, any>) => (
+					<ServerComponent<A> actor={actor} fn={prop as keyof ActorHandle<A>} {...props} />
+				);
+			},
+		});
+	}, [actor]);
+
+	return [{ actor, ...rest }, rsc] as const;
 }
