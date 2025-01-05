c38d89ba8eb6682485a1231029c6dd9d8399b0f7
Nathan Flurry <developer@nathanflurry.com>
2025-01-05 14:30:03 -0800

    fix(release): correct quotes on release please commit message

--- scripts/release/release_please.ts
+++ scripts/release/release_please.ts
@@ -5,5 +5,5 @@ export async function configureReleasePlease(opts: ReleaseOpts) {
 	// Check if the Release-As commit already exists
 	const commitMessage = `chore: release ${opts.version}`;
 	$.logStep("Updating Release Please", commitMessage);
-	await $`git commit --allow-empty -m "${commitMessage}" -m "Release-As: ${opts.version}"`;
+	await $`git commit --allow-empty -m ${commitMessage} -m ${`Release-As: ${opts.version}`}`;
 }
