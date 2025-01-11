772557e269ad515b0509a359b3e578b22e180f20
MasterPtato <maxpopkov.contact@gmail.com>
2025-01-11 20:05:57 +0000

    chore(mm): disable job run drain all

--- packages/services/job-run/src/workers/drain_all.rs
+++ packages/services/job-run/src/workers/drain_all.rs
@@ -3,16 +3,17 @@ use proto::backend::pkg::*;
 
 #[worker(name = "job-run-drain-all")]
 async fn worker(ctx: &OperationContext<job_run::msg::drain_all::Message>) -> GlobalResult<()> {
-	chirp_workflow::compat::workflow(
-		ctx,
-		crate::workflows::drain_all::Input2 {
-			nomad_node_id: ctx.nomad_node_id.clone(),
-			drain_timeout: ctx.drain_timeout,
-		},
-	)
-	.await?
-	.dispatch()
-	.await?;
+	// TODO: Temporarily disabled until better draining logic is applied
+	// chirp_workflow::compat::workflow(
+	// 	ctx,
+	// 	crate::workflows::drain_all::Input2 {
+	// 		nomad_node_id: ctx.nomad_node_id.clone(),
+	// 		drain_timeout: ctx.drain_timeout,
+	// 	},
+	// )
+	// .await?
+	// .dispatch()
+	// .await?;
 
 	Ok(())
 }
