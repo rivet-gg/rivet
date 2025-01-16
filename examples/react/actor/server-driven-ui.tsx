32ee110cd1444a19e3185d42f1c0ba8add206a6e
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2025-01-16 02:52:27 +0100

    chore(sdks): proper build system

--- examples/react/actor/server-driven-ui.tsx
+++ examples/react/actor/server-driven-ui.tsx
@@ -1,5 +1,5 @@
 import type { Rpc } from "@rivet-gg/actor";
-import { RscActor } from "@rivet-gg/actor/unstable-react";
+// import { RscActor } from "@rivet-gg/actor/unstable-react";
 
 export default class ServerDrivenUi extends RscActor {
 	messages(_rpc: Rpc<this>, props: Record<string, unknown>) {
