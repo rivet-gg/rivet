b4d7fd7b23697a2f86684e1e7042f3e8c7288fb7
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2025-01-17 09:58:20 +0100

    fix: remove react scan

--- site/src/app/(v2)/layout.tsx
+++ site/src/app/(v2)/layout.tsx
@@ -1,12 +1,15 @@
 import { EmbedDetector } from "@/components/EmbedDetector";
 import { Footer } from "@/components/Footer";
 import "@/styles/v2.css";
+import { Suspense } from "react";
 
 export default function Layout({ children }) {
 	return (
 		<>
 			{children}
-			<EmbedDetector />
+			<Suspense>
+				<EmbedDetector />
+			</Suspense>
 			<Footer />
 		</>
 	);
