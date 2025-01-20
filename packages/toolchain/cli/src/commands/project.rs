4c42fe91064844e75661646d9cb5bb282f544fd8
Nathan Flurry <developer@nathanflurry.com>
2025-01-19 18:58:24 -0800

    feat: add project and environment view commands

--- packages/toolchain/cli/src/commands/project.rs
+++ packages/toolchain/cli/src/commands/project.rs
@@ -1,29 +1,20 @@
 use anyhow::*;
 use clap::Subcommand;
 
-mod select;
-
 #[derive(Subcommand)]
 pub enum SubCommand {
-	#[clap(alias = "s")]
-	Select(select::Opts),
 	#[clap(alias = "v")]
-	View {
-		#[clap(long, alias = "env", short = 'e')]
-		environment: Option<String>,
-	},
+	View,
 }
 
 impl SubCommand {
 	pub async fn execute(&self) -> Result<()> {
 		match &self {
-			SubCommand::Select(opts) => opts.execute().await,
-			SubCommand::View { environment } => {
+			SubCommand::View => {
 				let ctx = toolchain::toolchain_ctx::load().await?;
-				let env = crate::util::env::get_or_select(&ctx, environment.as_ref()).await?;
 
 				let url = format!(
-					"{hub}/projects/{proj}/environments/{env}",
+					"{hub}/projects/{proj}",
 					hub = ctx.bootstrap.origins.hub,
 					proj = ctx.project.name_id,
 				);
