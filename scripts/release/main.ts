88308028f1ac1da5bde161d5bb19cbf1da31b96a
Nathan Flurry <developer@nathanflurry.com>
2025-02-03 17:45:13 -0800

    chore: release 25.1.0-rc.2
    Release-As: 25.1.0-rc.2

--- scripts/release/main.ts
+++ scripts/release/main.ts
@@ -125,7 +125,7 @@ async function main() {
 		assert(!args.noValidateGit, "cannot push without git validation");
 		$.logStep("Pushing Commits", "");
 		await $.logGroup(async () => {
-			const branch = (await $`git rev-parse --abbrev-ref HEAD`).stdout.trim();
+			const branch = (await $`git rev-parse --abbrev-ref HEAD`.stdout("piped")).stdout.trim();
 			if (branch === "main") {
 				// Push on main
 				await $`git push`;
