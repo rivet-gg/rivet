a736d0077c29b4691b35ef0e9329b37a8828c68e
MasterPtato <maxpopkov.contact@gmail.com>
2025-02-19 20:14:58 +0000

    feat: add fdb cli

--- packages/common/server-cli/src/commands/mod.rs
+++ packages/common/server-cli/src/commands/mod.rs
@@ -1,5 +1,6 @@
 pub mod config;
 pub mod db;
+pub mod fdb;
 pub mod provision;
 pub mod start;
 pub mod storage;
--- packages/edge/services/ds/src/keys/mod.rs
+++ packages/edge/services/ds/src/keys/mod.rs
@@ -5,5 +5,5 @@ pub mod port;
 pub mod server;
 
 pub fn subspace() -> fdb::tuple::Subspace {
-	fdb::tuple::Subspace::all().subspace(&("ds"))
+	fdb::tuple::Subspace::all().subspace(&("rivet", "ds"))
 }
