d6d6c7aafe558263c04d6faf9131154eff0e9391
Nathan Flurry <git@nathanflurry.com>
2025-03-07 10:35:11 +0000

    chore(pools): remove cert hack for clickhouse

--- packages/common/pools/src/db/clickhouse.rs
+++ packages/common/pools/src/db/clickhouse.rs
@@ -10,23 +10,11 @@ pub fn setup(config: Config) -> Result<Option<ClickHousePool>, Error> {
 	if let Some(clickhouse) = &config.server().map_err(Error::Global)?.clickhouse {
 		tracing::debug!("clickhouse connecting");
 
-		// Build HTTP connector
+		// Build HTTP client
 		let mut http_connector = hyper::client::connect::HttpConnector::new();
 		http_connector.enforce_http(false);
 		http_connector.set_keepalive(Some(Duration::from_secs(15)));
-
-		// Build TLS connector
-		let tls_connector = tokio_native_tls::native_tls::TlsConnector::builder()
-			// HACK(RVT-4649): We don't have ClickHouse certs on the edge server, this is temporary
-			// until we no longer need it on the edge
-			.danger_accept_invalid_certs(true)
-			.build()
-			.unwrap();
-
-		// Build HTTPs connector
-		let https_connector =
-			hyper_tls::HttpsConnector::from((http_connector, tls_connector.into()));
-
+		let https_connector = hyper_tls::HttpsConnector::new_with_connector(http_connector);
 		let http_client = hyper::Client::builder()
 			.pool_idle_timeout(Duration::from_secs(2))
 			.build(https_connector);
