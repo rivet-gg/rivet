391708da0ffd16349f0ac3f3d1414757677e4d40
jog1t <39823706+jog1t@users.noreply.github.com>
2025-03-10 19:02:47 +0000

    fix(hub): force context creation on lobbies page (#2161)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- frontend/apps/hub/src/routes/_authenticated/_layout/projects/$projectNameId/environments/$environmentNameId/lobbies.tsx
+++ frontend/apps/hub/src/routes/_authenticated/_layout/projects/$projectNameId/environments/$environmentNameId/lobbies.tsx
@@ -1,9 +1,14 @@
 import { ErrorComponent } from "@/components/error-component";
 import * as Layout from "@/domains/project/layouts/matchmaker-layout";
+import { projectsByGroupQueryOptions } from "@/domains/project/queries";
+import { guardUuids } from "@/lib/guards";
+import { ls } from "@/lib/ls";
+import { safeAsync } from "@rivet-gg/components";
 import {
 	type ErrorComponentProps,
 	Outlet,
 	createFileRoute,
+	notFound,
 } from "@tanstack/react-router";
 
 function MatchmakerLayoutErrorComponent(props: ErrorComponentProps) {
@@ -44,4 +49,29 @@ export const Route = createFileRoute(
 	component: MatchmakerLayoutView,
 	errorComponent: MatchmakerLayoutErrorComponent,
 	pendingComponent: Layout.Root.Skeleton,
+	beforeLoad: async ({
+		location,
+		context: { queryClient, auth, environment },
+		params: { projectNameId, environmentNameId },
+	}) => {
+		await guardUuids({
+			location,
+			queryClient,
+			projectNameId,
+			environmentNameId,
+		});
+
+		const [response] = await safeAsync(
+			queryClient.fetchQuery(projectsByGroupQueryOptions()),
+		);
+		const project = response?.games.find((p) => p.nameId === projectNameId);
+
+		if (!project) {
+			throw notFound();
+		}
+
+		ls.recentTeam.set(auth, project.developer.groupId);
+
+		return { project, environment };
+	},
 });
