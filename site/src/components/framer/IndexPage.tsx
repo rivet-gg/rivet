fd7fdf2d8946999c6b953820c868c348ddf1f9ee
NicholasKissel <28816635+NicholasKissel@users.noreply.github.com>
2025-03-13 21:41:34 +0000

    chore: website redesign (#2170)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- site/src/components/framer/IndexPage.tsx
+++ site/src/components/framer/IndexPage.tsx
@@ -2,19 +2,24 @@
 import "@/generated/framer/styles.css";
 import "@/generated/framer/tokens.css";
 import IndexFramer from "@/generated/framer/index";
+import { useRouter } from "next/navigation";
+import { UnframerProvider } from "unframer";
 
 // This file is only used for `use client` directive
 
 export const FramerIndexPage = () => {
+	const router = useRouter();
 	return (
-		<IndexFramer.Responsive
-			style={{ width: "100%", background: "#000000" }}
-			variants={{
-				xl: "Desktop",
-				md: "Tablet",
-				sm: "Phone",
-				base: "Phone",
-			}}
-		/>
+		<UnframerProvider navigate={router.push}>
+			<IndexFramer.Responsive
+				style={{ width: "100%", background: "#000000" }}
+				variants={{
+					xl: "Desktop",
+					md: "Tablet",
+					sm: "Phone",
+					base: "Phone",
+				}}
+			/>
+		</UnframerProvider>
 	);
 };
--- site/src/components/framer/SalesPage.tsx
+++ site/src/components/framer/SalesPage.tsx
@@ -1,41 +1,45 @@
 "use client";
 import SalesFramer from "@/generated/framer/sales";
-
+import { useRouter } from "next/navigation";
 import { usePostHog } from "posthog-js/react";
+import { UnframerProvider } from "unframer";
 
 // This file is only used for `use client` directive
 
 export const FramerSalesPage = () => {
 	const posthog = usePostHog();
+	const router = useRouter();
 	return (
-		<SalesFramer.Responsive
-			style={{ width: "100%", background: "#000000" }}
-			/* @ts-ignore */
-			onSubmit={(event) => {
-				event.preventDefault();
-				const formData = new FormData(event.target);
+		<UnframerProvider navigate={router.push}>
+			<SalesFramer.Responsive
+				style={{ width: "100%", background: "#000000" }}
+				/* @ts-ignore */
+				onSubmit={(event) => {
+					event.preventDefault();
+					const formData = new FormData(event.target);
 
-				const data = Object.fromEntries(
-					formData
-						.values()
-						.toArray()
-						.map((value, index) => [
-							`$survey_response${index > 0 ? `_${index}` : ""}`,
-							value.toString(),
-						]),
-				);
+					const data = Object.fromEntries(
+						formData
+							.values()
+							.toArray()
+							.map((value, index) => [
+								`$survey_response${index > 0 ? `_${index}` : ""}`,
+								value.toString(),
+							]),
+					);
 
-				posthog.capture("survey sent", {
-					$survey_id: "0193928a-4799-0000-8fc4-455382e21359",
-					...data,
-				});
-			}}
-			variants={{
-				xl: "Desktop",
-				md: "Tablet",
-				sm: "Phone",
-				base: "Phone",
-			}}
-		/>
+					posthog.capture("survey sent", {
+						$survey_id: "0193928a-4799-0000-8fc4-455382e21359",
+						...data,
+					});
+				}}
+				variants={{
+					xl: "Desktop",
+					md: "Tablet",
+					sm: "Phone",
+					base: "Phone",
+				}}
+			/>
+		</UnframerProvider>
 	);
 };
