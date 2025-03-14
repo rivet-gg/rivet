9d90db3a7d71e96c5bcc2a7beac77a8f976c59c5
NathanFlurry <git@nathanflurry.com>
2025-03-14 22:17:49 +0000

    chore: add sandboxed-code-execution demo for js & container (#2165)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

---
+++ examples/container/sandboxed-code-execution/fixtures/hello.ts
@@ -0,0 +1,8 @@
+/**
+ * User Code Example - Hello Script
+ * 
+ * This script logs a hello message.
+ * It is executed by the server when an HTTP request is received.
+ */
+
+console.log("Hello from user code!");
---
+++ examples/javascript/sandboxed-code-execution/README.md
@@ -0,0 +1,11 @@
+# JS Upload Script
+
+This demo uploads a script to Rivet and runs an actor from it.
+
+## Testing
+
+Run:
+
+```
+tsx scripts/upload_js.ts
+```
