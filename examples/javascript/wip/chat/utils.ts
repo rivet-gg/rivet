76a3602fc118627c7717468da62ce8c6caff953a
NathanFlurry <git@nathanflurry.com>
2025-03-14 22:17:50 +0000

    chore: flatten examples (#2181)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- examples/javascript/wip/chat/utils.ts
+++
@@ -1,7 +0,0 @@
-import { UserError } from "@rivet-gg/actor";
-
-export function validateUsername(username: string) {
-	if (username.length > 16) {
-		throw new UserError("Username too long");
-	}
-}
