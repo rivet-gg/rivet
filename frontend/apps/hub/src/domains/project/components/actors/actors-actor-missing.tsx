eb1e9d729f6671ddec52568dca7f91416ba8cd0b
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2025-03-23 23:12:36 +0100

    fix(hub): display fold buttons when actor is not selected

--- frontend/apps/hub/src/domains/project/components/actors/actors-actor-missing.tsx
+++ frontend/apps/hub/src/domains/project/components/actors/actors-actor-missing.tsx
@@ -1,13 +1,25 @@
+import { Button } from "@rivet-gg/components";
+import { useActorsLayout } from "./actors-layout-context";
 import { ActorsSidebarToggleButton } from "./actors-sidebar-toggle-button";
 
 export function ActorsActorMissing() {
+	const { setFolded, isFolded } = useActorsLayout();
 	return (
 		<div className="flex-1 h-full min-h-0 overflow-auto flex flex-col">
-			<div className="flex justify-between items-center border-b min-h-9 mt-2">
+			<div className="flex justify-between items-center border-b pt-2">
 				<ActorsSidebarToggleButton />
 			</div>
-			<div className="flex flex-1 items-center justify-center text-xs text-center flex-col gap-1">
+			<div className="flex-1 flex items-center justify-center text-xs text-center flex-col gap-1">
 				Please select an Actor from the list.
+				{isFolded ? (
+					<Button
+						variant="outline"
+						size="sm"
+						onClick={() => setFolded(false)}
+					>
+						Show Actor List
+					</Button>
+				) : null}
 			</div>
 		</div>
 	);
