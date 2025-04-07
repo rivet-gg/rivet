42779556f9fa3641d77f76f7b0d442de3e98d69b
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2025-04-08 00:22:35 +0200

    fix(site): deployment

--- site/scripts/buildcf.sh
+++ site/scripts/buildcf.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -euf
 
-yarn
+CWD=$(pwd)
+if [ ! -d "../../actor-core" ]; then
+    git clone --depth=1 --branch 04-04-fix_inspector git@github.com:rivet-gg/actor-core.git ../../actor-core
+fi
+cd ../../actor-core 
+yarn install
+yarn build
+cd $CWD
+yarn install
 yarn update-framer || true  # TODO: fix the actual error
 npx next build && cp _redirects out/_redirects
