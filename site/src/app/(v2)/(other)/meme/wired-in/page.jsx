884c02ecab62fe61ea90c8e45fe244924650a93b
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2025-04-07 22:09:16 +0200

    feat(site): upgrade to App Router and Nextjs 15.2

---
+++ site/src/app/(v2)/(other)/meme/wired-in/page.jsx
@@ -0,0 +1,14 @@
+export default function Index() {
+  return (
+    <div>
+      <iframe
+        className='h-screen w-full'
+        width='100%'
+        height='100vh'
+        src='https://www.youtube-nocookie.com/embed/PRBSKaTDrqQ?si=5jlDUh7aXIYev7Ty&amp;controls=0'
+        title='YouTube video player'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        allowfullscreen></iframe>
+    </div>
+  );
+}
