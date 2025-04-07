c57a5579aeab7e2e51d11076e849abc6ecacb92c
jog1t <39823706+jog1t@users.noreply.github.com>
2025-04-07 22:36:21 +0000

    fix(site): invalid actors import leaking to site (#2330)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- frontend/packages/components/src/hooks/index.ts
+++ frontend/packages/components/src/hooks/index.ts
@@ -1,2 +1,1 @@
 export * from "./use-breakpoint";
-export * from "./use-dialog";
--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,48 +1,45 @@
 // Auto-generate navigation
 // TODO Move to server component
-import "./scripts/generateNavigation.mjs";
+import './scripts/generateNavigation.mjs';
 
-import path from "node:path";
-import url from "node:url";
-import nextMDX from "@next/mdx";
-import { config } from "./src/mdx/mdx.mjs";
+import path from 'node:path';
+import url from 'node:url';
+import nextMDX from '@next/mdx';
+import { config } from './src/mdx/mdx.mjs';
 
 const withMDX = nextMDX(config);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export",
-	reactStrictMode: true,
-	transpilePackages: ["@rivet-gg/components", "@rivet-gg/icons"],
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-	pageExtensions: ["js", "jsx", "ts", "tsx", "mdx", "md"],
-	images: {
-		// For static output
-		unoptimized: true,
-	},
-	experimental: {
-		scrollRestoration: true,
-	},
-	webpack: (config) => {
-		const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-		return {
-			...config,
-			resolve: {
-				...config.resolve,
-				fallback: {
-					"react/jsx-dev-runtime": path.resolve(
-						__dirname,
-						"../node_modules/react/jsx-dev-runtime.js",
-					),
-					react: path.resolve(__dirname, "../node_modules/react"),
-				},
-			},
-		};
-	},
+  output: 'export',
+  reactStrictMode: true,
+  transpilePackages: ['@rivet-gg/components', '@rivet-gg/icons'],
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'],
+  images: {
+    // For static output
+    unoptimized: true
+  },
+  experimental: {
+    scrollRestoration: true
+  },
+  webpack: config => {
+    const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        fallback: {
+          'react/jsx-dev-runtime': path.resolve(__dirname, '../node_modules/react/jsx-dev-runtime.js'),
+          react: path.resolve(__dirname, '../node_modules/react')
+        }
+      }
+    };
+  }
 };
 
 export default async function () {
-	return withMDX(nextConfig);
+  return withMDX(nextConfig);
 }
