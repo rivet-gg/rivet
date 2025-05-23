4f10a0d8287bcae06c0b01c232d3234fe9f9921f
MasterPtato <maxpopkov.contact@gmail.com>
2025-05-23 23:22:17 +0000

    fix(pb): get new actor ids working e2e

--- packages/edge/infra/guard/core/src/lib.rs
+++ packages/edge/infra/guard/core/src/lib.rs
@@ -3,7 +3,6 @@ pub mod metrics;
 pub mod proxy_service;
 mod server;
 pub mod types;
-pub mod util;
 
 pub use cert_resolver::CertResolverFn;
 pub use proxy_service::{MiddlewareFn, ProxyService, ProxyState, RouteTarget, RoutingFn};
