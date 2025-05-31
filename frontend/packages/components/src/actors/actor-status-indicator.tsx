09c23f3f02efac1b8ed317611be4f926d505dea4
jog1t <39823706+jog1t@users.noreply.github.com>
2025-05-31 18:36:46 +0000

    feaft(hub): add tooltip to actor's status indicator (#2487)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- frontend/packages/components/src/actors/actor-status-indicator.tsx
+++ frontend/packages/components/src/actors/actor-status-indicator.tsx
@@ -2,6 +2,7 @@ import { Ping, cn } from "@rivet-gg/components";
 import type { Actor, ActorAtom } from "./actor-context";
 import { useAtomValue } from "jotai";
 import { selectAtom } from "jotai/utils";
+import type { ComponentPropsWithRef } from "react";
 
 export type ActorStatus =
 	| "starting"
@@ -34,33 +35,51 @@ export function getActorStatus(
 	return "unknown";
 }
 
+interface AtomizedActorStatusIndicatorProps
+	extends ComponentPropsWithRef<"span"> {
+	actor: ActorAtom;
+}
+
 export const AtomizedActorStatusIndicator = ({
 	actor,
-}: {
-	actor: ActorAtom;
-}) => {
+	...props
+}: AtomizedActorStatusIndicatorProps) => {
 	const status = useAtomValue(selectAtom(actor, selector));
-	return <ActorStatusIndicator status={status} />;
+	return <ActorStatusIndicator status={status} {...props} />;
 };
 
 const selector = ({ status }: Actor) => status;
 
-interface ActorStatusIndicatorProps {
+interface ActorStatusIndicatorProps extends ComponentPropsWithRef<"span"> {
 	status: ReturnType<typeof getActorStatus>;
 }
 
-export const ActorStatusIndicator = ({ status }: ActorStatusIndicatorProps) => {
+export const ActorStatusIndicator = ({
+	status,
+	...props
+}: ActorStatusIndicatorProps) => {
 	if (status === "running") {
-		return <Ping variant="success" className="relative right-auto" />;
+		return (
+			<Ping
+				variant="success"
+				{...props}
+				className={cn("relative right-auto", props.className)}
+			/>
+		);
 	}
 
 	return (
-		<div
-			className={cn("size-2 rounded-full", {
-				"bg-blue-600 animate-pulse": status === "starting",
-				"bg-destructive": status === "crashed",
-				"bg-foreground/10": status === "stopped",
-			})}
+		<span
+			{...props}
+			className={cn(
+				"size-2 rounded-full",
+				{
+					"bg-blue-600 animate-pulse": status === "starting",
+					"bg-destructive": status === "crashed",
+					"bg-foreground/10": status === "stopped",
+				},
+				props.className,
+			)}
 		/>
 	);
 };
