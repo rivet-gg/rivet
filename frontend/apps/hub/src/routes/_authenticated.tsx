85a5c84d2702b56ec8ed4ecdef2a383c325ed7bc
jog1t <39823706+jog1t@users.noreply.github.com>
2025-05-31 18:36:59 +0000

    fix(hub): close modal after creating a team (#2498)
    <!-- Please make sure there is an issue that this PR is correlated to. -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->

--- frontend/apps/hub/src/routes/_authenticated.tsx
+++ frontend/apps/hub/src/routes/_authenticated.tsx
@@ -77,6 +77,7 @@ function Modals() {
 					await navigate({
 						to: "/projects/$projectNameId",
 						params: { projectNameId: data.nameId },
+						search: { modal: undefined },
 					})
 				}
 				dialogProps={{
@@ -89,6 +90,7 @@ function Modals() {
 					await navigate({
 						to: "/teams/$groupId",
 						params: { groupId: data.groupId },
+						search: { modal: undefined },
 					})
 				}
 				dialogProps={{
