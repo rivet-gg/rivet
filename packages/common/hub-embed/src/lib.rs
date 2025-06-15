97e3ac2fbec89d31e4112e2482b51db1dc13a607
Nathan Flurry <git@nathanflurry.com>
2025-06-15 22:03:46 +0000

    chore: replace `hub-embed` with proxy to 127.0.0.1:5080 for /ui/ path in dev

--- packages/common/hub-embed/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use include_dir::{include_dir, Dir};
-
-const HUB_DIR: Dir = include_dir!("$HUB_PATH");
-
-pub fn get_file_content(path: &str) -> Option<&'static [u8]> {
-	HUB_DIR.get_file(path).map(|file| file.contents())
-}
