2b4037b5e6e1ad582f60ea7ffd47404f71b703f2
MasterPtato <maxpopkov.contact@gmail.com>
2025-06-24 01:04:32 +0000

    feat: add actor kv to runners

--- packages/edge/services/pegboard/db/runner/migrations/20200101000000_init.up.sql
+++ packages/edge/services/pegboard/db/runner/migrations/20200101000000_init.up.sql
@@ -4,15 +4,13 @@ CREATE TABLE IF NOT EXISTS actor_runners (
     runner_id UUID,
     started_at DateTime64 (9),
     finished_at DateTime64 (9)
-) ENGINE = ReplicatedMergeTree ()
+) ENGINE = ReplicatedReplacingMergeTree ()
 PARTITION BY
     toStartOfHour (started_at)
 ORDER BY (
     actor_id,
     generation,
-    runner_id,
-    toUnixTimestamp (started_at),
-    toUnixTimestamp (finished_at)
+    runner_id
 )
 TTL toDate (started_at + toIntervalDay (30))
 SETTINGS index_granularity = 8192, ttl_only_drop_parts = 1;
