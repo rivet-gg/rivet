f291cf87736e084e84a7ebba1cf884392acc4be3
Nathan Flurry <developer@nathanflurry.com>
2025-07-15 14:08:31 -0700

    chore: fix vercel routing

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,42 +1,45 @@
-import path from 'node:path';
-import url from 'node:url';
-import nextMDX from '@next/mdx';
-import { config } from './src/mdx/mdx';
+import path from "node:path";
+import url from "node:url";
+import nextMDX from "@next/mdx";
+import { config } from "./src/mdx/mdx";
 
 const withMDX = nextMDX(config);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: false,
-  reactStrictMode: true,
-  transpilePackages: ['@rivet-gg/components', '@rivet-gg/icons'],
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'],
-  images: {
-    // For static output
-    unoptimized: true
-  },
-  experimental: {
-    scrollRestoration: true
-  },
-  webpack: config => {
-    const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        fallback: {
-          'react/jsx-dev-runtime': path.resolve(__dirname, '../node_modules/react/jsx-dev-runtime.js'),
-          react: path.resolve(__dirname, '../node_modules/react')
-        }
-      }
-    };
-  }
+	output: "export",
+	trailingSlash: true, // Required for Vercel
+	reactStrictMode: true,
+	transpilePackages: ["@rivet-gg/components", "@rivet-gg/icons"],
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	pageExtensions: ["js", "jsx", "ts", "tsx", "mdx", "md"],
+	images: {
+		// For static output
+		unoptimized: true,
+	},
+	experimental: {
+		scrollRestoration: true,
+	},
+	webpack: (config) => {
+		const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+		return {
+			...config,
+			resolve: {
+				...config.resolve,
+				fallback: {
+					"react/jsx-dev-runtime": path.resolve(
+						__dirname,
+						"../node_modules/react/jsx-dev-runtime.js",
+					),
+					react: path.resolve(__dirname, "../node_modules/react"),
+				},
+			},
+		};
+	},
 };
 
 export default async function () {
-  return withMDX(nextConfig);
+	return withMDX(nextConfig);
 }
