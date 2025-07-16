6f25cffdd76bd87685b9605e7ef1c8e940892e88
Nathan Flurry <developer@nathanflurry.com>
2025-07-16 12:47:10 -0700

    chore: clean up mdx components

--- frontend/packages/components/src/cta-card.tsx
+++ frontend/packages/components/src/cta-card.tsx
@@ -1,5 +1,6 @@
 import { Icon, faArrowRight } from "@rivet-gg/icons";
 import { ActionCard, type ActionCardProps } from "./action-card";
+import type { PropsWithChildren } from "react";
 
 export interface CtaCardProps extends Omit<ActionCardProps, "action"> {}
 
@@ -9,5 +10,11 @@ export const CtaCard = (props: CtaCardProps) => {
 			{...props}
 			action={<Icon icon={faArrowRight} className="size-4" />}
 		/>
+	);
+};
+
+export const CardGroup = ({ children }: PropsWithChildren) => {
+	return (
+		<div className="not-prose grid gap-4 md:grid-cols-2">{children}</div>
 	);
 };
